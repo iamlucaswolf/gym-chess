26221fcabc9a229cc521366afb46adfbc02863e2
Lucas Wolf <iamlucaswolf@gmail.com>
2020-05-31 14:39:06 +0200

    Automatically regsiter Chess-v0 env

--- gym_chess/__init__.py
+++ gym_chess/__init__.py
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from gym.envs.registration import register
+from gym_chess.envs import Chess
+
+
+def _make_env(encode=False):
+    env = Chess()
+    return env
+
 
 register(
     id='Chess-v0',
-    entry_point='gym_chess.envs:Chess'
+    entry_point='gym_chess:_make_env',
 )
